003cb30c398c00f668ba80137c4129e585e280f2
Him-anshu-Dev10 <himutech10@gmail.com>
2026-06-12 16:21:05 +0530

    try

--- admin-saloon/vite.config.ts
+++ admin-saloon/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/admin-saloon/',
   plugins: [react()],
   optimizeDeps: {
     include: ['chart.js', 'react-chartjs-2'],
